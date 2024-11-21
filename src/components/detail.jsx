46f09596cb91ccd6f3085a05d52f03eb47e994cf
issblann <anaisabelpp@outlook.com>
2024-11-21 17:40:49 -0500

    agrego lista y detalles

--- src/components/detail.jsx
+++ src/components/detail.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react';
 
-const Detail = ({ station }) => {
+const Detail = () => {
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-between bg-white rounded-lg shadow-md overflow-hidden">
-      
         <div className="lg:w-1/2 p-6 text-gray-800">
-          <h1 className="text-2xl font-bold mb-4">{station.name}</h1>
+          <h1 className="text-2xl font-bold mb-4">name</h1>
           <p className="text-gray-600 mb-4">
-            Ubicación:{" "}
+            Ubicación:{' '}
             <a
-              href={station.mapLink}
+              // href={station.mapLink}
               className="text-blue-500 hover:underline"
               target="_blank"
               rel="noopener noreferrer"
@@ -18,29 +17,28 @@ const Detail = ({ station }) => {
               Ver en el mapa
             </a>
           </p>
-          <p className="text-gray-700 mb-4">{station.description}</p>
+          <p className="text-gray-700 mb-4">desc</p>
           <p className="text-gray-600 font-semibold">
-            Estado:{" "}
+            Estado:{' '}
             <span
-              className={`${
-                station.status === "Disponible" ? "text-green-500" : "text-red-500"
-              }`}
+            // className={`${
+            //   station.status === "Disponible" ? "text-green-500" : "text-red-500"
+            // }`}
             >
-              {station.status}
+              estatus
             </span>
           </p>
         </div>
 
-    
         <div className="lg:w-1/2 p-6 flex flex-col items-center">
           <img
-            src={station.image}
-            alt={`Imagen de ${station.name}`}
+            // src={station.image}
+            // alt={`Imagen de ${station.name}`}
             className="w-full h-64 object-cover rounded-md shadow-md mb-6"
           />
           <button
             className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-700 transition duration-200"
-            onClick={() => alert(`Reserva hecha para ${station.name}`)}
+            // onClick={() => alert(`Reserva hecha para ${station.name}`)}
           >
             Reservar
           </button>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../components/home';
 import About from '../components/home';
+import List from '../components/list';
+import Detail from '../components/detail';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/descubre" element={<List />} /> */}
+      <Route path="/descubre" element={<List />} />
+      <Route path="/detalles" element={<Detail />} />
     </Routes>
   );
 };
