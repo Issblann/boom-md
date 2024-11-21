24d1938ed5ebe88ec5bd0aeae0a666a3da86e619
Ana Milena Vasquez <mileana0910@gmail.com>
2024-11-21 17:27:54 -0500

    Se agrega list y detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
-
+import List from './component-list/list'
+import Detail from './component-list/detail'
 function App() {
   return (
     <>
       <h1 className="text-red-400">BIKE</h1>
+      <List />
+      <Detail />
     </>
   );
 }
